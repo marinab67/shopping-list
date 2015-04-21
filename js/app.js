9ec2f95980b64d4527f714df3c0cadbb6ca26234
marinab67 <marinab67@gmail.com>
2015-04-21 00:44:48 -0400

    final

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 function addItem () { 
 	var newItem = $(".new-item").val();
-	var newEl = '<li class="new"><input type="checkbox" class="done">'+newItem+' <button class="delete">delete</button></li>';
+	var newEl = '<li class="new"><input type="checkbox" class="done">'+newItem+' <button class="delete">x</button></li>';
 	$(".items ul").append(newEl);
 	$(".new-item").val("");
 }
@@ -10,11 +10,21 @@ function deleteItem () {
 }
 
 function doneItem () { 
+	if ($(this).parent().css('textDecoration') == 'line-through') { 
+		$(this).parent().css('textDecoration', 'none').css('color', 'white').css('font-style', 'normal');
+	} else {
+
 	$(this).parent().css('textDecoration', 'line-through').css('color', 'gray').css('font-style', 'italic');
+	}
 }
 
 $(document).on("ready", function() {
 	$("#add").on("click", addItem); 
-	$(".delete").on("click", deleteItem);
-	$(".done").on("click", doneItem);
+	$(".textbox").keyDown(function(event) {
+  		if(event.keyCode == 13) {addItem}
+	});
+	//$(".delete").on("click", deleteItem);
+	//$(".done").on("click", doneItem);
+	$(document).on("click", ".done", doneItem);
+	$(document).on("click", ".delete", deleteItem);
 });
